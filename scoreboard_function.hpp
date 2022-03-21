819cbb66d0712973d91cf8587d78a0086356b37a
Hoang The Anh <101539204+htainvn@users.noreply.github.com>
2022-03-21 11:13:55 +0700

    New function implemented.
    importScore function is completed, waiting for checking.

--- scoreboard_function.hpp
+++ scoreboard_function.hpp
@@ -3,13 +3,15 @@
 //  structure
 //
 //  Created by Hoang The Anh on 19/03/2022.
-//
+//  Edited by Hoang The Anh on 21/03/2022.
 
 #ifndef scoreboard_function_hpp
 #define scoreboard_function_hpp
 
 #include <stdio.h>
+#include "structure.h"
 
 void exportStudentInCourse(Course* someRandomCourse);
+void importScore(Course*& course, string filename);
 
 #endif /* scoreboard_function_hpp */
