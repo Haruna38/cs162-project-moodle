a4d8a11b9085c6596a1d83fa56166634ffe3c433
Hoang The Anh <101539204+htainvn@users.noreply.github.com>
2022-03-21 11:16:21 +0700

    Some general functions
    2 two functions: GetSemester and GetSchoolYear. GetSemester needed more information.

---
+++ general_function.cpp
@@ -0,0 +1,20 @@
+//
+//  general_function.cpp
+//  structure
+//
+//  Created by Hoang The Anh on 21/03/2022.
+//
+
+#include "general_function.hpp"
+#include <fstream>
+
+int getCurrentSchoolYear() {
+    std::ifstream fin;
+    fin.open("schoolyear.txt");
+    int schoolyear_result = 0;
+    while (fin.eof()) {
+        fin >> schoolyear_result;
+    }
+    fin.close();
+    return schoolyear_result;
+}
---
+++ general_function.hpp
@@ -0,0 +1,14 @@
+//
+//  general_function.hpp
+//  structure
+//
+//  Created by Hoang The Anh on 21/03/2022.
+//
+
+#ifndef general_function_hpp
+#define general_function_hpp
+
+int getCurrentSchoolYear();
+//int getSemester();
+
+#endif /* general_function_hpp */
