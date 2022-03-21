8b31838952179e815182282d2f66f1d3c005a587
Hoang The Anh <101539204+htainvn@users.noreply.github.com>
2022-03-21 11:15:11 +0700

    New function implemented.
    importScore function is completed. Waiting for checking.

--- scoreboard_function.cpp
+++ scoreboard_function.cpp
@@ -3,15 +3,17 @@
 //  structure
 //
 //  Created by Hoang The Anh on 19/03/2022.
-//
+//  Edited by Hoang The Anh on 22/03/2022.
 
 #include "scoreboard_function.hpp"
 #include "structure.h"
+#include "general_function.hpp"
 #include <fstream>
+#include <sstream>
 
 void exportStudentInCourse(Course* someRandomCourse) {
     ofstream fout;
-    string filename = "Student_" + someRandomCourse->courseID + "_" + to_string(getCurrentSchoolYear()) + "_" + to_string(getSemester()) + ".csv";
+    string filename = "Student_" + someRandomCourse->courseID + "_" + to_string(getCurrentSchoolYear()) + "_" + /*to_string(getSemester()) +*/ ".csv";
 
     fout.open(filename);
     fout << "No,First Name,Last Name,\n";
@@ -24,4 +26,25 @@ void exportStudentInCourse(Course* someRandomCourse) {
         fout << desc;
     }
     fout.close();
+}
+
+void importScore(Course* &course, string filename) {
+    ifstream fin;
+    fin.open(filename);
+    string s;
+    getline(fin, s);
+    while (getline(fin, s)) {
+        stringstream sst(s);
+        int studentID;  char dumm;
+        sst >> studentID >> dumm >> studentID >> dumm;
+        score* newScore = new score();
+        newScore->studentInform->studentID = studentID;
+        while ((sst >> studentID).fail() && !sst.eof()) {
+            string dummy;
+            sst >> dummy;
+        }
+        fin >> dumm >> newScore->totalMark >> dumm >> newScore->finalMark >> dumm >> newScore->midtermMark >> dumm >> newScore->otherMark;
+        course->courseScoreBoard.addNewScore(newScore);
+    }
+    fin.close();
 }
