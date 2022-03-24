72562f798a69da7b6de3788ff8f4ea12b447babb
dinh-quang-hieu <101045977+dinh-quang-hieu@users.noreply.github.com>
2022-03-24 14:04:57 +0700

    Update Create school year .cpp

--- Create school year .cpp
+++ Create school year .cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include<fstream> 
-#include"Function.h"
 using namespace std;
 void inputSchoolYear( ofstream& fout) {
 	int x;
 	cout << "Enter your current school year : ";
 	cin >> x;
-	fout.open("schoolyear.txt");
+	fout.open("schoolyear.txt",ios::app);
 	fout << x << endl;
 	fout.close();
 }
