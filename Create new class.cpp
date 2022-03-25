7983cc8d87fc175a283953db37ae288092244c8a
dinh-quang-hieu <101045977+dinh-quang-hieu@users.noreply.github.com>
2022-03-25 23:46:18 +0700

    Update Create new class.cpp

--- Create new class.cpp
+++ Create new class.cpp
@@ -2,46 +2,26 @@
 ///Started 17/03/2022
 //Edited 22/03/2022
 //Edited 23/03/2022
+//Edited 25/03/2022
+//Add student to list class new 
 #include <iostream>
-#include"class.h"
 #include<string.h>
+#include "general_function.h"
 #include<string>
 #include<fstream>
+#include"struct.h"
 using namespace std;
-void createClassList(Class*& pClass , int numsClass) {
-	ifstream fin;
-	fin.open("Class.txt");
-    pClass = new Class;
-    Class *pCur = pClass;
-    for (int i = 0; i < numsClass && pCur != nullptr; i++) {
-	fin >> pCur->name;
-        pCur = pCur->next;
-    }
-}
-
-void createNewClass(Class *&pClass, int &numsClass) {
-	pClass->name = "";
-	char ans[10];
-	bool tmp = true;
-	ofstream fout;
-	numsClass = 1;
-	fout.open("Class.txt");
-	while (tmp != false) {
-		cout << "Input your class's name : ";
-		cin.get();
-		getline(cin, pClass->name, '\n');
-		fout << pClass->name;
-		cout << "You want to create a new class again ?" << endl;
-		cout << "If yes input Y , no input N : ";
-		cin.getline(ans, 10, '\n');
-		if (ans == "Y") {
-			tmp = true;
-			numsClass++;
+void createClassList(School HCMUS) {
+	int schoolyear_x = getCurrentSchoolYear(); 
+	Class* newClass = new Class();
+	cin >> newClass->classID;
+	newClass->schoolyear = schoolyear_x;
+		bool status = true;
+		while (status) {
+			Student* newStudent = new Student();
+			cin >> newStudent->No >> newStudent->studentID >> newStudent->dateOfBirth >> newStudent->socialID >> newStudent->firstName >>newStudent->lastName >>newStudent->gender;
+			newClass->addNewStudent(newStudent);
+			if (/*xuly*/) status = false;
 		}
-		else if (!strcmp(ans, "N")) {
-			tmp = false;
-		}
+		HCMUS.allClasses.addNewClass(newClass);
 	}
-	fout.close();
-}
-
