0385464ba9f990f0b9141593ebf29a3fd02d35e7
dinh-quang-hieu <101045977+dinh-quang-hieu@users.noreply.github.com>
2022-03-27 22:25:54 +0700

    Update Create new class.cpp

--- Create new class.cpp
+++ Create new class.cpp
@@ -3,6 +3,7 @@
 //Edited 22/03/2022
 //Edited 23/03/2022
 //Edited 25/03/2022
+//Edited 27/03/2022
 //Add student to list class new 
 #include <iostream>
 #include<string.h>
@@ -16,12 +17,25 @@ void createClassList(School HCMUS) {
 	Class* newClass = new Class();
 	cin >> newClass->classID;
 	newClass->schoolyear = schoolyear_x;
+	if (/*xuly*/) { //nhap tay
 		bool status = true;
 		while (status) {
 			Student* newStudent = new Student();
-			cin >> newStudent->No >> newStudent->studentID >> newStudent->dateOfBirth >> newStudent->socialID >> newStudent->firstName >>newStudent->lastName >>newStudent->gender;
+			cin >> newStudent->No >> newStudent->studentID >> newStudent->dateOfBirth >> newStudent->socialID >> newStudent->firstName >>newStudent->lastName>> newStudent->gender;
 			newClass->addNewStudent(newStudent);
 			if (/*xuly*/) status = false;
 		}
-		HCMUS.allClasses.addNewClass(newClass);
 	}
+	else { //nhap bang 
+		ifstream fin;
+		string filename; cin >> filename;
+		fin.open(filename);
+		while (!fin.eof()) {
+			Student* newStudent = new Student();
+			fin >> newStudent->No >> newStudent->studentID >> newStudent->dateOfBirth >> newStudent->socialID >> newStudent->firstName >> newStudent->lastName >> newStudent->gender;
+			newClass->addNewStudent(newStudent);
+		}
+	}
+	HCMUS.allClasses.addNewClass(newClass);
+
+}
