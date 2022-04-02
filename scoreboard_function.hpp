45884759b1e5a50e62a4e7ba51dfa3a24f83446b
Hoang The Anh <101539204+htainvn@users.noreply.github.com>
2022-04-02 14:18:01 +0700

    Update scoreboard_function.hpp

--- scoreboard_function.hpp
+++ scoreboard_function.hpp
@@ -6,6 +6,7 @@
 //  Edited by Hoang The Anh on 21/03/2022.
 //  Edited by Hoang The Anh on 22/03/2022.
 //  Edited by Hoang The Anh on 23/03/2022.
+//  Edited by Hoang The Anh on 02/04/2022.
 
 #ifndef scoreboard_function_hpp
 #define scoreboard_function_hpp
@@ -14,6 +15,7 @@
 #include "structure.h"
 
 void exportStudentInCourse(Course* someRandomCourse);
+void exportScore(Course* randomCourse);
 void importScore(Course*& course, string filename);
 void seeScoreOfCourse(Course* course);
 void updateStudentResult(Course*& course, int studentID, float totalMark_x, float finalMark_x, float midtermMark_x, float otherMark_x);
