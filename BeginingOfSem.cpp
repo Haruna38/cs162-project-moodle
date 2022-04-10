fa45f62ffa333dc604bc5e68be880b235aac8313
phungptn <ngothaiphung412@gmail.com>
2022-04-10 12:12:25 +0700

    delete unnecessary code

--- BeginingOfSem.cpp
+++ BeginingOfSem.cpp
@@ -1,14 +1,6 @@
 
 #include "BeginingOfSem.h"
 #include "structure.h"
-//void gotoXY(int x, int y)
-//{
-//	static HANDLE h = NULL;
-//	if (!h)
-//		h = GetStdHandle(STD_OUTPUT_HANDLE);
-//	COORD c = { x, y };
-//	SetConsoleCursorPosition(h, c);
-//}
 
 void createSemester() {
 	int schoolyear;
