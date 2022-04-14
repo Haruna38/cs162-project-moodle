516375374089599368a7e035389f546db461ba6a
Hoang The Anh <101539204+htainvn@users.noreply.github.com>
2022-04-14 10:34:40 +0700

    Global file for variable

---
+++ global.cpp
@@ -0,0 +1,4 @@
+#include "global.h"
+
+School mySchool = School();
+int current_id = 0;
---
+++ global.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "cs162-project-moodle-main/structure.h"
+
+#ifndef GLOBAL
+#define GLOBAL
+
+extern School mySchool;
+extern int current_id;
+
+#endif
