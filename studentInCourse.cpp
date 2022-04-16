7630d6cf68198ff6952811306e61d7314b4c87a3
nxhoa <59250517+nxhoa@users.noreply.github.com>
2022-04-16 13:06:24 +0900

    attempt to fix bugs

--- studentInCourse.cpp
+++ studentInCourse.cpp
@@ -2,20 +2,20 @@
 
 bool enroll(Student *student, string courseID_x) { // Returns 'true' if success, 'false' if failed
     if (student == NULL) return false;
-    enrolledCourse *course = student->myCourses->monitor;
+    enrolledCourse *course = student.myCourses->monitor->nextEnrolledCourse;
     while (courses != NULL) {
         if (courses->courseID == courseID_x) return false;
         course = course->nextEnrolledCourse;
     }
-    student->myCourses->addNewEnrolledCourse(course);
+    student.myCourses->addNewEnrolledCourse(course);
     return true;
 }
 
 bool remove(Student *student, string courseID_x) { // Returns 'true' if success, 'false' if failed
     if (student == NULL) return false;
-    enrolledCourse *course = student->myCourses->monitor, *start = new enrolledCourse, *prev = start;
+    enrolledCourse *prev = student.myCourses->monitor, *start = prev->nextEnrolledCourse;
     if (course == NULL) return false;
-    start->nextEnrolledCOurse = course;
+    start->nextEnrolledCourse = course;
     bool removed = false;
     while (course != NULL) {
         enrolledCourse *inter = course;
@@ -28,24 +28,22 @@ bool remove(Student *student, string courseID_x) { // Returns 'true' if success,
         }
         else prev = inter;
     }
-    delete student->myCourses->monitor;
-    student->myCourses->monitor = start;
     return removed;
 }
 
 bool viewAllEnrolledCourses(Student *student) {
-    if (student == NULL || student->myCourses == NULL) {
+    if (student == NULL || student.myCourses == NULL) {
         cout << "Error: Student not found or could not get courses info!" << endl;
         return;
     }
-    enrolledCourses courseList = student->myCourses->monitor;
+    enrolledCourses courseList = student.myCourses->monitor->next;
     cout << "Student's Enrolled Courses:" << endl;
     if (courseList == NULL) return;
-    courseList = courseList->nextCourse;
+    courseList = courseList->nextEnrolledCourse;
     int i = 0;
     while (courseList != NULL) {
         courseList->displayInfo(++i);
-        courseList = courseList->nextCourse;
+        courseList = courseList->nextEnrolledCourse;
     }
-    if (student->myCourses->sizeofList == 0) cout << "No courses to display." << endl;
+    if (student.myCourses->sizeofList == 0) cout << "No courses to display." << endl;
 }
