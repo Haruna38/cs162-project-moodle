059609645ba63bfce0484110eafe118f6c38476d
Hoang The Anh <101539204+htainvn@users.noreply.github.com>
2022-04-18 16:12:03 +0700

    Add files via upload

---
+++ addnewclass.h
@@ -0,0 +1,37 @@
+#ifndef ADDNEWCLASS_H
+#define ADDNEWCLASS_H
+
+#include <QMainWindow>
+#include "cs162-project-moodle-main/structure.h"
+
+namespace Ui {
+class AddNewClass;
+}
+
+class AddNewClass : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit AddNewClass(QWidget *parent = nullptr);
+    ~AddNewClass();
+    Ui::AddNewClass *ui;
+
+private slots:
+
+    void on_addStudentBtt_clicked();
+
+    void on_tableWidget_cellClicked(int row, int column);
+
+    void on_tableWidget_itemSelectionChanged();
+
+    void on_saveBtt_clicked();
+
+    void on_uploadFileBtt_clicked();
+
+private:
+    std::string openFile();
+    void createClassListByFile(School& mySchool, string classID_x, string filename);
+};
+
+#endif // ADDNEWCLASS_H
---
+++ addnewstudent.cpp
@@ -0,0 +1,62 @@
+#include "addnewstudent.h"
+#include "ui_addnewstudent.h"
+#include "addnewclass.h"
+#include "ui_addnewclass.h"
+
+Ui::AddNewClass* ancUI_x;
+
+#include <QTableWidgetItem>
+
+addnewstudent::addnewstudent(QWidget *parent, Ui::AddNewClass* ancUI) :
+    QDialog(parent),
+    ui(new Ui::addnewstudent)
+{
+    ui->setupUi(this);
+    ancUI_x = ancUI;
+}
+
+addnewstudent::~addnewstudent()
+{
+    delete ui;
+}
+
+void addnewstudent::on_pushButton_clicked()
+{
+    long long studentID_x = (ui->studentIDEdit->text()).toLongLong();
+    std::string firstname_x = (ui->firstNameEdit->text()).toStdString();
+    std::string lastname_x = (ui->lastNameEdit->text()).toStdString();
+    std::string gender_x = (ui->genderBox->currentText()).toStdString();
+    std::string date_x = (ui->birthdateEdit->date()).toString("dd/MM/yyyy").toStdString();
+    long long socialID_x = (ui->socialIDEdit->text()).toLongLong();
+    ancUI_x->tableWidget->insertRow(ancUI_x->tableWidget->rowCount());
+    QTableWidgetItem* item;
+    for (int i = 0; i < ancUI_x->tableWidget->columnCount(); i++) {
+        item = new QTableWidgetItem;
+        switch(i) {
+            case 0:
+                item->setText(QString::number(ancUI_x->tableWidget->rowCount()));
+                break;
+            case 1:
+                item->setText(QString::number(studentID_x));
+                break;
+            case 2:
+                item->setText(QString::fromUtf8(firstname_x));
+                break;
+            case 3:
+                item->setText(QString::fromUtf8(lastname_x));
+            break;
+            case 4:
+                item->setText(QString::fromUtf8(gender_x));
+                break;
+            case 5:
+                item->setText(QString::fromUtf8(date_x));
+                break;
+            case 6:
+                item->setText(QString::number(socialID_x));
+                break;
+        }
+        ancUI_x->tableWidget->setItem(ancUI_x->tableWidget->rowCount()-1, i, item);
+    }
+    this->close();
+}
+
