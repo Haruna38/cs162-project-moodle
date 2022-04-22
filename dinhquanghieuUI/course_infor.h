1de4affa46dab95cef04267bfedad7802f24012f
dinh-quang-hieu <101045977+dinh-quang-hieu@users.noreply.github.com>
2022-04-22 13:21:36 +0700

    Add files via upload

---
+++ dinhquanghieuUI/course_infor.h
@@ -0,0 +1,23 @@
+#ifndef COURSE_INFOR_H
+#define COURSE_INFOR_H
+#include"structure.h"
+#include <QMainWindow>
+
+namespace Ui {
+class course_infor;
+}
+
+class course_infor : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit course_infor(QWidget *parent = nullptr, Course* course = NULL);
+    ~course_infor();
+
+private:
+    Ui::course_infor *ui;
+    Course *thisCourse;
+};
+
+#endif // COURSE_INFOR_H
