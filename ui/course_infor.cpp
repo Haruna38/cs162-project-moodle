cc3679381f3ef67b16245ebf5bcc39bca26b5c86
dinh-quang-hieu <101045977+dinh-quang-hieu@users.noreply.github.com>
2022-04-26 15:58:14 +0700

    Create course_infor.cpp

---
+++ ui/course_infor.cpp
@@ -0,0 +1,21 @@
+#include "ui/course_infor.h"
+#include "ui_course_infor.h"
+#include "operating_function/structure.h"
+
+course_infor::course_infor(QWidget *parent , Course* course) :
+    QMainWindow(parent),
+    ui(new Ui::course_infor)
+{
+    ui->setupUi(this);
+    thisCourse = course;
+    ui->course_id->setText(QString::fromUtf8(thisCourse->courseID));
+    ui->course_name->setText(QString::fromUtf8(thisCourse->courseName));
+    ui->teacher_course->setText(QString::fromUtf8(thisCourse->teacherName));
+    ui->num_student->setText(QString::number(thisCourse->maxStudent));
+    ui->numOfCredit->setText(QString::number(thisCourse->numberofCredit));
+}
+
+course_infor::~course_infor()
+{
+    delete ui;
+}
